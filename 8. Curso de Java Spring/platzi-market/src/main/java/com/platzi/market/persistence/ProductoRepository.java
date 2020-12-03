d49bdcc268c964c7bca4452bc1fb4c2166fdc8c0
Cristian Ortega <c.ortegamu@gmail.com>
2020-12-03 12:32:07 -0300

    preparacion del proyecto para su despliegue en heroku

--- 8. Curso de Java Spring/platzi-market/src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ 8. Curso de Java Spring/platzi-market/src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.platzi.market.domain.repository.ProductRepository;
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
 import com.platzi.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,8 +13,10 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
-
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
     private ProductMapper mapper;
 
     @Override
@@ -24,30 +27,29 @@ public class ProductoRepository implements ProductRepository {
 
     @Override
     public Optional<List<Product>> getByCategory(int categoryId) {
-        List<Producto> productos = productoCrudRepository.findByIdCategoriaOOrderByNombreAsc(categoryId);
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
         return Optional.of(mapper.toProducts(productos));
     }
 
     @Override
     public Optional<List<Product>> getScarseProducts(int quantity) {
         Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
-        return productos.map( prods -> mapper.toProducts(prods));
+        return productos.map(prods -> mapper.toProducts(prods));
     }
 
     @Override
     public Optional<Product> getProduct(int productId) {
-        return productoCrudRepository.findById(productId).map(prod -> mapper.toProduct(prod));
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
     }
 
     @Override
     public Product save(Product product) {
-        return mapper.toProduct(productoCrudRepository.save(mapper.toProducto(product)));
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
     }
 
     @Override
-    public void delete(int id) {
-        productoCrudRepository.deleteById(id);
+    public void delete(int productId) {
+        productoCrudRepository.deleteById(productId);
     }
-
-
 }
--- 8. Curso de Java Spring/platzi-market/src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
+++ 8. Curso de Java Spring/platzi-market/src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
@@ -2,7 +2,7 @@ package com.platzi.market.persistence.mapper;
 
 import com.platzi.market.domain.Product;
 import com.platzi.market.persistence.entity.Producto;
-import org.mapstruct.InheritConfiguration;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -11,22 +11,19 @@ import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {CategoryMapper.class})
 public interface ProductMapper {
-
     @Mappings({
             @Mapping(source = "idProducto", target = "productId"),
             @Mapping(source = "nombre", target = "name"),
-            @Mapping(source = "categoryId", target = "categoryId"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
             @Mapping(source = "precioVenta", target = "price"),
             @Mapping(source = "cantidadStock", target = "stock"),
             @Mapping(source = "estado", target = "active"),
-            @Mapping(source = "categoria", target = "category")
+            @Mapping(source = "categoria", target = "category"),
     })
     Product toProduct(Producto producto);
-
     List<Product> toProducts(List<Producto> productos);
 
-    @InheritConfiguration
+    @InheritInverseConfiguration
     @Mapping(target = "codigoBarras", ignore = true)
     Producto toProducto(Product product);
-
 }
